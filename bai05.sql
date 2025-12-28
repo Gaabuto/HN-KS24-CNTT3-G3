d26e5d8b6ff5c07ea0e9621d3f2c00d1d0bb6f22
CJustin23 <annt.b24dtcn181@stu.ptit.edu.vn>
2025-12-28 23:43:34 +0700

    Refactor SQL tables for student and subjects

--- bai05.sql
+++ bai05.sql
@@ -1,22 +1,25 @@
 create database exercise05;
 use exercise05;
 
-create table student (
-	student_id int primary key auto_increment,
-    student_name varchar(50) not null
+CREATE TABLE Student (
+    student_id VARCHAR(10) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL
 );
 
-create table subjects(
-	subject_id int primary key auto_increment,
-    subject_name varchar(50) not null
+CREATE TABLE Subject (
+    subject_id VARCHAR(10) PRIMARY KEY,
+    subject_name VARCHAR(100) NOT NULL
 );
 
-create table score (
-	student_id int not null,
-    subject_id int not null,
-    progress_score decimal(4,2) not null check(progress_score between 0 and 10),
-    final_score decimal(4,2) not null check(final_score between 0 and 10),
-    primary key (student_id, subject_id),
-    foreign key (student_id) references student(student_id),
-    foreign key (subject_id) references subjects(subject_id)
-)
+CREATE TABLE Score (
+    student_id VARCHAR(10),
+    subject_id VARCHAR(10),
+    process_score DECIMAL(4,2) CHECK (process_score BETWEEN 0 AND 10),
+    final_score DECIMAL(4,2) CHECK (final_score BETWEEN 0 AND 10),
+
+    PRIMARY KEY (student_id, subject_id),
+    FOREIGN KEY (student_id)
+	REFERENCES Student(student_id),
+    FOREIGN KEY (subject_id)
+	REFERENCES Subject(subject_id)
+);
