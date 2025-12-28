bb360a48635e153be514feb5f8dfea43cce50d59
CJustin23 <annt.b24dtcn181@stu.ptit.edu.vn>
2025-12-28 22:23:57 +0700

    Update bai04.sql

--- bai04.sql
+++ bai04.sql
@@ -1,17 +1,17 @@
-create database bai4;
-use bai4;
+create database exercise04;
+use exercise04;
 
 create table teacher (
-    teacher_id varchar(60) primary key,
-    full_name varchar(80) not null,
-    email varchar(80) not null unique
+	teacher_id int primary key auto_increment,
+    teacher_name varchar(50) not null,
+    teacher_email varchar(50) not null unique
 );
 
-create table teaching (
-    teacher_id varchar(60) not null,
-    class_id varchar(60) not null,
-    primary key (teacher_id, class_id),
-    foreign key (teacher_id) references teacher(teacher_id),
-    foreign key (class_id) references class(class_id)
+create table subjects(
+	subject_id int primary key auto_increment,
+    subject_name varchar(50) not null,
+    subject_credit int not null check(subject_credit > 0)
 );
 
+alter table subjects add teacher_id int;
+alter table subjects add constraint subjects_teacher foreign key (teacher_id) references teacher(teacher_id);
