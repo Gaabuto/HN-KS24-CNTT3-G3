89c950ea566d2e53daae65534a8a20490a8ccb1d
Gaabuto <khanh040604@gmail.com>
2025-12-28 22:31:51 +0700

    Revert "Refactor database and table structures in bai05.sql"
    This reverts commit 2012ef6ce256ddb2035e5bfbec7f324dfcffd643.

--- bai05.sql
+++ bai05.sql
@@ -1,22 +1,26 @@
-create database exercise05;
-use exercise05;
+create database bai5;
+use bai5;
 
 create table student (
-	student_id int primary key auto_increment,
-    student_name varchar(50) not null
+    student_id varchar(60)   primary key,  
+    full_name varchar(90) not null           
 );
 
-create table subjects(
-	subject_id int primary key auto_increment,
-    subject_name varchar(50) not null
+create table class (
+    class_id varchar(60) primary key,
+    class_name varchar(60) not null,
+    credit int not null check (credit > 0)
 );
 
 create table score (
-	student_id int not null,
-    subject_id int not null,
-    progress_score decimal(4,2) not null check(progress_score between 0 and 10),
-    final_score decimal(4,2) not null check(final_score between 0 and 10),
-    primary key (student_id, subject_id),
+    student_id varchar(50) not null,
+    class_id varchar(50) not null,
+    
+    midterm_score decimal(4,2) check (midterm_score between 0 and 10),
+    final_score decimal(4,2) check (final_score between 0 and 10),
+    recorded_date date not null default '2000-01-01', -- giá trị tạm thời cho ngày tháng và năm
+    primary key (student_id, class_id),
+    
     foreign key (student_id) references student(student_id),
-    foreign key (subject_id) references subjects(subject_id)
-)
+    foreign key (class_id)   references class(class_id)
+);
