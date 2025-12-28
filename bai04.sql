aee42a51e15c4965bb85c76d3b5cedddf026bbe2
CJustin23 <annt.b24dtcn181@stu.ptit.edu.vn>
2025-12-28 23:43:01 +0700

    Create Teacher and Subject tables with relationships

--- bai04.sql
+++ bai04.sql
@@ -1,17 +1,22 @@
 create database exercise04;
 use exercise04;
 
-create table teacher (
-	teacher_id int primary key auto_increment,
-    teacher_name varchar(50) not null,
-    teacher_email varchar(50) not null unique
+CREATE TABLE Subject (
+    subject_id VARCHAR(10) PRIMARY KEY,
+    subject_name VARCHAR(100) NOT NULL,
+    credit INT CHECK (credit > 0)
 );
 
-create table subjects(
-	subject_id int primary key auto_increment,
-    subject_name varchar(50) not null,
-    subject_credit int not null check(subject_credit > 0)
+CREATE TABLE Teacher (
+    teacher_id VARCHAR(10) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE
 );
 
-alter table subjects add teacher_id int;
-alter table subjects add constraint subjects_teacher foreign key (teacher_id) references teacher(teacher_id);
+ALTER TABLE Subject
+ADD teacher_id VARCHAR(10);
+
+ALTER TABLE Subject
+ADD CONSTRAINT fk_subject_teacher
+FOREIGN KEY (teacher_id)
+REFERENCES Teacher(teacher_id);
