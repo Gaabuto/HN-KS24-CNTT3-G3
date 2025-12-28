39d4be3e392cc94540975e9afc8c54f4b0256fc5
CJustin23 <annt.b24dtcn181@stu.ptit.edu.vn>
2025-12-28 22:22:25 +0700

    Update bai01.sql

--- bai01.sql
+++ bai01.sql
@@ -1,21 +1,17 @@
-DROP TABLE IF EXISTS sinh_vien;
-DROP TABLE IF EXISTS lop_hoc;
+create database exercise01;
+use exercise01;
 
-CREATE TABLE lop_hoc (
-  ma_lop   VARCHAR(20) PRIMARY KEY,
-  ten_lop  VARCHAR(50) NOT NULL,
-  nam_hoc  INT NOT NULL,
-  CONSTRAINT ck_nam_hoc CHECK (nam_hoc >= 2000)
-) ENGINE=InnoDB;
+create table class(
+	class_id int primary key auto_increment,
+    class_name varchar(100) not null,
+    class_year int not null
+);
 
-CREATE TABLE sinh_vien (
-  ma_sinh_vien VARCHAR(20) PRIMARY KEY,
-  ho_ten       VARCHAR(100) NOT NULL,
-  ngay_sinh    DATE NOT NULL,
-  ma_lop       VARCHAR(20) NOT NULL,
-
-  CONSTRAINT fk_sv_lop
-    FOREIGN KEY (ma_lop) REFERENCES lop_hoc(ma_lop)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-) ENGINE=InnoDB;
+create table student(
+	student_id int primary key auto_increment,
+    student_name varchar(100) not null,
+    student_date date not null,
+    class_id int not null,
+    foreign key(class_id) references class(class_id)
+);
+drop table student;
