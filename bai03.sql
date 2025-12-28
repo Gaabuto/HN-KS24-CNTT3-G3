3eb0ea0fb4f2378eb54ce9ba143b8575fcfb690f
Gaabuto <khanh040604@gmail.com>
2025-12-28 22:32:40 +0700

    Revert "Refactor database schema and create new tables"
    This reverts commit 64adba9f8980de48d3d41e6e379f75b78b86c713.

--- bai03.sql
+++ bai03.sql
@@ -1,22 +1,21 @@
-create database exercise03;
-use exercise03;
+create database bai3;
+use bai3;
 
+create table class (
+    class_id varchar(60) primary key,
+    class_name varchar(60) not null,
+    credit int not null check (credit > 0)
+);
 create table student (
-	student_id int primary key auto_increment,
-    student_name varchar(50) not null
+    student_id varchar(60) primary key,
+    student_name varchar(60) not null
 );
-
-create table subjects(
-	subject_id int primary key auto_increment,
-    subject_name varchar(50) not null,
-    subject_credit int not null check(subject_credit > 0)
-);
-
 create table enrollment (
-	student_id int not null,
-    subject_id int not null,
-    enrollment_date date not null,
-    primary key (student_id, subject_id),
-    foreign key(student_id) references student(student_id),
-    foreign key(subject_id) references subjects(subject_id)
-)
+    student_id varchar(60) not null,
+    class_id varchar(60) not null,
+    register_date date not null,
+    primary key (student_id, class_id),
+    
+    foreign key (student_id) references student(student_id),
+    foreign key (class_id) references class(class_id)
+);
